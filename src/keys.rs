d2ba49939afbb4ee6e5de1e5c974a3d2bf0e79ee
Jambo <48104113+Jambo2486@users.noreply.github.com>
2023-06-26 21:48:24 +0100

    Create keys.rs

---
+++ src/keys.rs
@@ -0,0 +1,17 @@
+pub enum Key {
+	Character(char),
+	Enter,
+	Backspace,
+	Escape,
+	// Add more keys
+}
+
+pub fn byte_to_key(byte: u8) -> Option<Key> {
+	match byte {
+		byte if byte.is_ascii_graphic() => Some(Key::Character(byte as char)),
+		b'\n' => Some(Key::Enter),
+        b'\x1B' => Some(Key::Escape),
+        b'\x7F' => Some(Key::Backspace),
+		_ => None,
+	}
+}
