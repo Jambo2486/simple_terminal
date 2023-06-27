3267029b99cbb3abdb9497d9b009bdcc6af2f28c
Jambo <48104113+Jambo2486@users.noreply.github.com>
2023-06-27 14:17:33 +0100

    Update keys.rs

--- src/keys.rs
+++ src/keys.rs
@@ -10,6 +10,7 @@ pub fn byte_to_key(byte: u8) -> Option<Key> {
 	match byte {
 		byte if byte.is_ascii_graphic() => Some(Key::Character(byte as char)),
 		b'\n' => Some(Key::Enter),
+		b'\x0a' => Some(Key::Enter),
 		b'\x1B' => Some(Key::Escape),
 		b'\x7F' => Some(Key::Backspace),
 		_ => None,
