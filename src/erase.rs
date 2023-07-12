15be1dc7424338a2aa19a0c04b04612f1785da8b
Jambo <48104113+Jambo2486@users.noreply.github.com>
2023-07-12 19:25:41 +0100

    Update erase.rs

--- src/erase.rs
+++ src/erase.rs
@@ -1,25 +1,24 @@
-use crate::csi::csi;
 use crate::known_csi;
 
 /// Does not move cursor
-pub fn line() {
-	known_csi!('K', "2");
+pub fn line() -> String {
+	known_csi!('K', "2")
 }
 
 /// Erases from the cursor to the end of the current line
 /// Does not move cursor
-pub fn line_after_cursor() {
-	csi('K', &["0".to_string()]);
+pub fn line_after_cursor() -> String {
+	known_csi!('K', "0")
 }
 
 /// Erases from the beginning of the current line to the cursor
 /// Does not move cursor
-pub fn line_before_cursor() {
-	csi('K', &["1".to_string()]);
+pub fn line_before_cursor() -> String {
+	known_csi!('K', "1")
 }
 
 /// Moves cursor to (0,0)
 /// Does not clear scrollback buffer
-pub fn screen() {
-	csi('J', &["2".to_string()]);
+pub fn screen() -> String {
+	known_csi!('J', "2")
 }
