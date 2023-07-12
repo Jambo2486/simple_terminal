f235cda63cadeca1f5aac88af48be3574dfa3c71
Jambo <48104113+Jambo2486@users.noreply.github.com>
2023-07-12 19:21:34 +0100

    Update cursor.rs

--- src/cursor.rs
+++ src/cursor.rs
@@ -10,51 +10,56 @@ fn parse_pos_2d(column: usize, line: usize) -> [String; 2] {
 	[parse_pos(column), parse_pos(line)]
 }
 
-pub fn set_position(column: usize, line: usize) {
-	csi('H', &parse_pos_2d(column, line));
+pub fn set_position(column: usize, line: usize) -> String {
+	csi('H', &parse_pos_2d(column, line))
 }
 
-pub fn move_to_column(column: usize) {
-	csi('G', &[parse_pos(column)]);
+pub fn move_to_column(column: usize) -> String {
+	csi('G', &[parse_pos(column)])
 }
 
-pub fn move_right(columns: usize) {
-	csi('C', &[columns.to_string()]);
+pub fn move_right(columns: usize) -> String {
+	csi('C', &[columns.to_string()])
 }
 
-pub fn move_left(columns: usize) {
-	csi('D', &[columns.to_string()]);
+pub fn move_left(columns: usize) -> String {
+	csi('D', &[columns.to_string()])
 }
 
-pub fn move_up(lines: usize) {
-	csi('A', &[lines.to_string()]);
+pub fn move_up(lines: usize) -> String {
+	csi('A', &[lines.to_string()])
 }
 
-pub fn move_down(lines: usize) {
-	csi('B', &[lines.to_string()]);
+pub fn move_down(lines: usize) -> String {
+	csi('B', &[lines.to_string()])
 }
 
-pub fn move_by(columns: isize, lines: isize) {
-	if columns > 0 {
-		move_right(columns as usize);
+/// Positive lines: up
+/// Negative lines: down
+pub fn move_by(columns: isize, lines: isize) -> [String; 2] {
+	let columns = if columns > 0 {
+		move_right(columns as usize)
 	} else if columns < 0 {
-		move_left(-columns as usize);
-	}
-	if lines > 0 {
-		move_up(lines as usize);
+		move_left(-columns as usize)
+	} else { String::new() };
+
+	let lines = if lines > 0 {
+		move_up(lines as usize)
 	} else if lines < 0 {
-		move_down(-lines as usize);
-	}
+		move_down(-lines as usize)
+	} else { String::new() };
+
+	[columns, lines]
 }
 
 /// To the beginning of the line, `lines` down
-pub fn next_line(lines: usize) {
-	csi('E', &[lines.to_string()]);
+pub fn next_line(lines: usize) -> String {
+	csi('E', &[lines.to_string()])
 }
 
 /// To the beginning of the line, `lines` up
-pub fn previous_line(lines: usize) {
-	csi('F', &[lines.to_string()]);
+pub fn previous_line(lines: usize) -> String {
+	csi('F', &[lines.to_string()])
 }
 
 // TODO:
