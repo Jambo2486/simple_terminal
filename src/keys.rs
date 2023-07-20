0c13969e4d7f21b4ea455c3c55c135f41802b723
Jambo <48104113+Jambo2486@users.noreply.github.com>
2023-07-20 14:49:17 +0100

    Add unknown key
    Replaced None with Key::Unknown; should be a bit more intuitive.

--- src/keys.rs
+++ src/keys.rs
@@ -4,15 +4,16 @@ pub enum Key {
 	Backspace,
 	Escape,
 	// Add more keys
+	Unknown
 }
 
 pub fn byte_to_key(byte: u8) -> Option<Key> {
 	match byte {
 		byte if byte.is_ascii_graphic() => Some(Key::Character(byte as char)),
-		b'\n' => Some(Key::Enter),
-		b'\x0a' => Some(Key::Enter),
-		b'\x1B' => Some(Key::Escape),
-		b'\x7F' => Some(Key::Backspace),
-		_ => None,
+		b'\n' => Key::Enter,
+		b'\x0a' => Key::Enter,
+		b'\x1B' => Key::Escape,
+		b'\x7F' => Key::Backspace,
+		_ => Key::Unknown,
 	}
 }
